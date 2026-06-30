47ee1b9dc6320f60101b76ba409006644009f9f8
钟有新 <dorsinmilitto@gmail.com>
2026-06-30 10:53:30 +0800

    SEO optimization: structured data, OG/Twitter cards, article meta, robots.txt fix, compressHTML

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.huaxiaink.com',
+  compressHTML: true,
   integrations: [sitemap({
     i18n: {
       defaultLocale: 'en',
